05d9f625eb00c4dd7773603f93c2ef03d2663851
Daniel Rodrigues de Carvalho Filho <danircfilho@hotmail.com>
2022-04-04 12:15:23 -0300

    fim-diretivas-estruturais

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diretivas-estruturais.component.scss']
 })
 export class DiretivasEstruturaisComponent implements OnInit {
-
+  /* ngIf */
   public condicao: boolean = true /* false não aparece na tela */
   public outraCondicao: boolean = true /* para exemplificar alteraCondicao */
   public clickMuda: boolean = true /* para exemplificar */
+
+  /* ngFor */
+  public list: Array<{ nome: string, idade: number}> = [
+    { nome: "Daniel Rodrigues", idade: 46},
+    { nome: "João da Silva", idade: 30},
+    { nome: "José", idade: 25}
+  ]
+
+  /* ngSwitch */
+  public nome: string = ''; /* pode ser setado um valor especifico de início ou não (vazio '') */
 
   constructor() { }
 
@@ -24,6 +34,9 @@ export class DiretivasEstruturaisComponent implements OnInit {
     }, 2000) /* 2000 milisegundos (2 segundos) */
   }
 
+  /* Funções a serem chamadas: */
+  /* ngFor e ngIf são "sintax sugar", uma sintaxe fácil para programar algoritimos de forma simples */
+  /* ngIf */
   public clicar() {
     if (this.clickMuda) {    
         this.clickMuda = false;
@@ -31,4 +44,16 @@ export class DiretivasEstruturaisComponent implements OnInit {
         this.clickMuda = true;
       }
   }
+
+  /* ngIf */
+  public clicAdicLista() {
+    /* o push server, por exemplo para adicionar itens em um carrinho de compras */
+    this.list.push({nome: "Adicionado", idade: 20}) /* push adiciona */ 
+  }
+
+  public clicRemovItem(event: number) {
+    /* o splice serve para remover um item, no caso esclolhido um array */
+    this.list.splice(event) /* splice remove */
+  }
+
 } 
